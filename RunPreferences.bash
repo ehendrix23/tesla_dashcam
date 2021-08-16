bd58cb4e0eb9c27816b7350752c073ea4affbffd
Erik Hendrix <hendrix_erik@hotmail.com>
2021-08-16 11:41:14 -0600

    Provide small script for running all preference files

--- RunPreferences.bash
+++ RunPreferences.bash
@@ -1,19 +1,59 @@
 #!/bin/bash
 
-Command="python /Users/ehendrix/Documents_local/GitHub/tesla_dashcam/tesla_dashcam/tesla_dashcam.py"
-# InputFolder="/Volumes/TeslaCam/TeslaCam/SavedClips"
-InputFolder="/Volumes/TeslaCam/TeslaCam/SentryClips/2020-09-19_*"
-OutputFolder="/Volumes/SD Card"
-PreferenceFolder="/Users/ehendrix/Documents_local/GitHub/tesla_dashcam/Preference_Files"
+# Small script that will run tesla_dashcam leveraging all the different preferences provided in the Preference_Files folder.
+
+# Folder(s) to scan for clips
+InputFolders='/Volumes/TeslaCam/TeslaCam/SentryClips /Volumes/TeslaCam/TeslaCam/SavedClips'
+
+# Folder to store resulting movie files.
+OutputFolder='/Volumes/TeslaCam/Movies'
+
+# Start Timestamp
+StartTimestamp=""
+# StartTimestamp="2020-09-19T13:29:10"
+
+# End Timestamp
+EndTimestamp=""
+# EndTimestamp="2020-09-19T13:29:30"
+
+# Path to folder containing the preference files
+PreferenceFolder="./Preference_Files"
+
+
+# LogLevel
+LogLevel=""
+# LogLevel="DEBUG"
+
+if [ -f tesla_dashcam/tesla_dashcam.py ]; then
+	Command="tesla_dashcam/tesla_dashcam.py"
+else
+	Command="tesla_dashcam.py"
+fi
+Command="python ${Command}"
+
+if [ "${OutputFolder}" != "" ]; then
+	if [ ! -d ${OutputFolder} ]; then
+		mkdir ${OutputFolder}
+	fi
+	OutputFolder="--output ""${OutputFolder}""" 
+fi
+
+if [ "${StartTimestamp}" != "" ]; then
+	StartTimestamp="--start_timestamp ${StartTimestamp}"
+fi
+if [ "${EndTimestamp}" != "" ]; then
+	EndTimestamp="--end_timestamp ${EndTimestamp}"
+fi
+
+if [ "${LogLevel}" != "" ]; then
+	LogLevel="--loglevel ${LogLevel}"
+fi
 
 for preferencefile in ${PreferenceFolder}/*; do
 	filename="${preferencefile##*/}"
 	folder=${filename%%.*}
 	echo "Using Preference File ${filename}"
-	if [ ! -d "${OutputFolder}/${folder}" ]; then
-		mkdir "${OutputFolder}/${folder}"
-	fi
-	${Command} "${InputFolder}" --loglevel DEBUG --output "${OutputFolder}/${folder}" @${preferencefile}
-	exit
+	echo "${Command} ${InputFolders} ${LogLevel} ${OutputFolder} @${preferencefile}  ${StartTimestamp} ${EndTimestamp}"
+	${Command} ${InputFolders} ${LogLevel} ${OutputFolder} @${preferencefile} ${StartTimestamp} ${EndTimestamp}
 done
 
