0e00c5cf6b1bc17fbe87f967dab915a0d1b646fe
magicalyak <tom.gamull@gmail.com>
2021-10-12 09:11:40 -0400

    add nvidia and vaapi options

--- examples/teslausbarchive.sh
+++ examples/teslausbarchive.sh
@@ -22,24 +22,24 @@ destroot="/mnt2/tesla_dashcam"
 sourceroot="/mnt2/tesla"
 
 echo "Looping through $sourceroot/ for teslacam video to move"
-for dirsource in ${sourceroot}/!(archive|processing)/; do
-	if [ -d ${dirsource} ]; then
-		sourcedirdate=$(basename $dirsource | cut -d'_' -f1)
-		dir_time=$(date -d $sourcedirdate +%s)
-		if [ $dir_time -le $(date -d 'now - 1 day' +%s) ] || [ $sourcedirdate == $(date -d 'now - 1 day' +%Y-%m-%d) ]; then
+for dirsource in "${sourceroot}"/!(archive|processing)/; do
+	if [ -d "${dirsource}" ]; then
+		sourcedirdate=$(basename "$dirsource" | cut -d'_' -f1)
+		dir_time=$(date -d "$sourcedirdate" +%s)
+		if [ "$dir_time" -le "$(date -d 'now - 1 day' +%s)" ] || [ "$sourcedirdate" == "$(date -d 'now - 1 day' +%Y-%m-%d)" ]; then
 			echo "$dirsource is over 24 hours old"
 			destpath="${destroot}/${sourcedirdate}"
 			if [ ! -d "$destpath" ]; then
 				echo "Creating $destpath"
-				mkdir $destpath;
+				mkdir "$destpath";
 			fi
 			processpath="${sourceroot}/processing/${sourcedirdate}"
 			if [ ! -d "$processpath" ]; then
 				echo "Creating $processpath"
-				mkdir $processpath;
+				mkdir "$processpath";
 			fi
-			echo "Moving $disource to processing directory $processpath"
-			mv $dirsource $processpath
+			echo "Moving $dirsource to processing directory $processpath"
+			mv "$dirsource" "$processpath"
 		fi
 	else
 		echo "No directories found to process!"
@@ -47,24 +47,24 @@ for dirsource in ${sourceroot}/!(archive|processing)/; do
 done
 
 echo "Converting Videos from processing folder with tesla_dashcam container"
-for dirprocess in ${sourceroot}/processing/*/; do
-	if [ -d ${dirprocess} ]; then
-		processdirdate=$(basename $dirprocess)
+for dirprocess in "${sourceroot}"/processing/*/; do
+	if [ -d "${dirprocess}" ]; then
+		processdirdate=$(basename "$dirprocess")
 		docker run --rm \
 		-e TZ=America/New_York \
-		-v $destroot/$processdirdate:/root/Videos \
-		-v $dirprocess:/root/Import \
+		-v $destroot/"$processdirdate":/root/Videos \
+		-v "$dirprocess":/root/Import \
 		magicalyak/tesla_dashcam \
 		--motion_only \
 		--merge /root/Import \
-		--output /root/Videos/$processdirdate.mp4
+		--output /root/Videos/"$processdirdate".mp4
 		echo "Archiving folder in $sourceroot/archive/$processdirdate"
-		mv $dirprocess $sourceroot/archive/$processdirdate
+		mv "$dirprocess" "$sourceroot"/archive/"$processdirdate"
 		echo "Removing intermediate files in $destroot/$processdirdate"
-		for file in ${destroot}/${processdirdate}/*; do
+		for file in "${destroot}"/"${processdirdate}"/*; do
 			if [[ $file == *"${processdirdate}T"* ]]; then
 				echo "Deleting $file"
-				rm -f $file
+				rm -f "$file"
 			fi
 		done
 	else
@@ -74,15 +74,15 @@ done
 
 
 echo "Removing directories in $sourceroot/archive if over 3 months and .preserve isn't present"
-for dirarchive in ${sourceroot}/archive/*/; do
-	archivedirdate=$(basename $dirarchive)
-	dir_time=$(date -d $archivedirdate +%s)
-	if [ $dir_time -le $(date -d 'now - 3 months' +%s) ]; then
+for dirarchive in "${sourceroot}"/archive/*/; do
+	archivedirdate=$(basename "$dirarchive")
+	dir_time=$(date -d "$archivedirdate" +%s)
+	if [ "$dir_time" -le "$(date -d 'now - 3 months' +%s)" ]; then
 		if [ -f "${dirarchive}/.preserve" ]; then
 			echo ".preserve file found in $dirarchive! Skipping..."
 		else
 			echo "Removing $dirarchive since it is over 3 months old"
-			rm -rf $dirarchive
+			rm -rf "$dirarchive"
 		fi
 	fi
 done
