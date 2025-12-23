08cd0599892632af4c60dddabbf8c561ebff1aa7
Erik Hendrix <hendrix_erik@hotmail.com>
2025-12-23 12:00:43 -0700

    Add creating of ZIP file for creating Windows executable.

--- bundles/Windows/create_executable.cmd
+++ bundles/Windows/create_executable.cmd
@@ -45,6 +45,30 @@ if %ERRORLEVEL% NEQ 0 (
 
 bundles\Windows\tesla_dashcam\tesla_dashcam.exe --version
 
+echo "Creating ZIP archive"
+setlocal enabledelayedexpansion
+set ZIP_NAME=bundles\Windows\Tesla_Dashcam.zip
+set ZIP_STAGING=build\Windows\zip_staging
+
+if exist "%ZIP_STAGING%" (
+  rmdir /s /q "%ZIP_STAGING%"
+)
+mkdir "%ZIP_STAGING%"
+
+REM Copy payload into staging
+copy bundles\Windows\tesla_dashcam\tesla_dashcam.exe "%ZIP_STAGING%\"
+copy "bundles\Windows\tesla_dashcam\Tesla_Dashcam - README.html" "%ZIP_STAGING%\"
+copy LICENSE.txt "%ZIP_STAGING%\"
+copy bundles\Windows\ffmpeg_LICENSE.txt "%ZIP_STAGING%\"
+
+REM Create ZIP using PowerShell
+powershell -Command "Compress-Archive -Path '%ZIP_STAGING%\*' -DestinationPath '%ZIP_NAME%' -Force"
+
+if %ERRORLEVEL% NEQ 0 (
+  echo "Failed to create ZIP archive."
+  goto :eof
+)
+
 echo All done.
 
 :eof
